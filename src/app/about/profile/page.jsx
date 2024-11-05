5421c2bcdc49ebbba07c0ea45175823f6807515c
enokrosanti <enokrosanti54@gmail.com>
2024-11-05 16:29:06 +0700

    layout dan template

--- src/app/about/profile/page.jsx
+++ src/app/about/profile/page.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "./navbar";
 
 export default function ProfilePage() {
   return (
     <div>
       <h1>ProfilePage</h1>
-      <Navbar></Navbar>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Navbar from "./navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello World
     </main>
-  )
+  );
 }
