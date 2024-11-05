4e47d671a259842dfc092f0aa78d8de6c847c5f1
enokrosanti <enokrosanti54@gmail.com>
2024-11-05 13:54:54 +0700

    setup project

---
+++ src/app/about/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./navbar";
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1>ProfilePage</h1>
+      <Navbar></Navbar>
+    </div>
+  );
+}
